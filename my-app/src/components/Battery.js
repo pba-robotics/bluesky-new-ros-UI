fdac54217e544b5a56ca918967c149fa0d4266fd
Jivooo <jovi.tan@pba.com.sg>
2023-11-07 14:21:16 +0800

    feat: allow changing of battery graph refs #3379

--- my-app/src/components/Battery.js
+++ my-app/src/components/Battery.js
@@ -1,4 +1,4 @@
-import { HStack, Box, Flex } from "@chakra-ui/react"
+import { HStack, Box, Flex, Button } from "@chakra-ui/react"
 import React from "react"
 import Plot from 'react-plotly.js'
 
@@ -6,40 +6,45 @@ import Plot from 'react-plotly.js'
 
 function Battery(props) {
 
-    console.log(props.batteryOutputHistory)
+    //console.log(props.batteryOutputHistory)
+    const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
 
     var time_data = props.batteryOutputHistory.map((x) => x.header.stamp.secs)
-    var percentage_data = props.batteryOutputHistory.map((x) => x.current)
+    var battery_data = props.batteryOutputHistory.map((x) => (
+        props.selectedBatteryParameter === 'Percentage' ? x.percentage : 
+        props.selectedBatteryParameter === 'Current' ? x.current : average(x.cell_temperature)
+        ))
+    var label = props.selectedBatteryParameter
 
-    console.log(time_data)
+    //console.log(time_data)
 
     var data = [
         {
              x: time_data,
-             y: percentage_data,
+             y: battery_data,
              fill: 'tozeroy',
              type: 'scatter',
-             name: 'Battery'
+             name: label
            }
            
     ];
 
-    const average = arr => arr.reduce( ( p, c ) => p + c, 0 ) / arr.length;
+   
     return(
         <Box>
             <HStack w='1024px' justifyContent='center'>
-                <Flex w='341px' justifyContent='center'>
+                <Button w='341px' h='auto' justifyContent='center' fontSize='42' value='Percentage' _hover={{bg: false}} bgColor={props.selectedBatteryParameter === "Percentage" ? 'aqua' : 'none'} onClick={props.onClick}>
                     Battery %:  <br/>
                     {(props.batteryOutput.percentage*100).toFixed(2)}
-                </Flex>
-                <Flex w='341px' justifyContent='center'>
+                </Button>
+                <Button w='341px' h='auto' justifyContent='center' fontSize='42' value='Current' _hover={{bg: false}} bgColor={props.selectedBatteryParameter === "Current" ? 'aqua' : 'none'} onClick={props.onClick}>
                     Current: <br/>
                     {props.batteryOutput.current.toFixed(2)} A
-                </Flex>
-                <Flex w='341px' justifyContent='center'>
+                </Button>
+                <Button w='341px' h='auto' justifyContent='center' fontSize='42' value='Temperature' _hover={{bg: false}} bgColor={props.selectedBatteryParameter === "Temperature" ? 'aqua' : 'none'} onClick={props.onClick}>
                     Temperature: <br/>
                     {average(props.batteryOutput.cell_temperature).toFixed(2) + ' \u00B0'}C
-                </Flex>
+                </Button>
             </HStack>
         <Plot
         data={data}
